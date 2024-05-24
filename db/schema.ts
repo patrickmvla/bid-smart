a54c6c7367a82e8364dc38d9f01a75e513b2b36d
Patrick Mvula <mvlapatrick@gmail.com>
2024-05-24 03:52:02 +0200

    auth.js

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,8 @@
 import { pgTable, serial } from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
 export const bids = pgTable("bids", {
-  id: serial("id").primaryKey()
-  
+  id: serial("id").primaryKey(),
 });
+
+export const insertBidsSchema = createInsertSchema(bids);
